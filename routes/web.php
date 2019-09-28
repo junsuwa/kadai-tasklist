a6dbb4633bd21ab7927072191596323bd5239bf6
Junichiro.Suwa <suwajun@gmail.com>
2019-09-28 13:48:21 +0000

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'TasksController@index');
+
+//CRUD
+Route::resource('tasks','TasksController');
